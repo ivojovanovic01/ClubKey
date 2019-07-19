885ae724d3777446b77f3facf6802f3d5b8d0152
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2019-07-19 23:30:45 +0200

    Created all Interfaces

---
+++ ClubKey/ClubKey.Domain/Repositories/Interfaces/IAchievementRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ClubKey.Data.Entities.Models;
+
+namespace ClubKey.Domain.Repositories.Interfaces
+{
+    public interface IAchievementRepository
+    {
+        List<Achievement> GetAllAchievements();
+        List<Achievement> GetAchievementsByUserId(int userId);
+    }
+}
---
+++ ClubKey/ClubKey.Domain/Repositories/Interfaces/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ClubKey.Data.Entities.Models;
+
+namespace ClubKey.Domain.Repositories.Interfaces
+{
+    public interface IEventRepository
+    {
+        int AddEvent(Event eventToAdd);
+        List<Event> GetTenEventsByCityId(int cityId);
+        List<Event> GetTenSimilarEvents(Event mainEvent);
+        List<Event> GetEventsByClubId(int clubId);
+        Event GetEventById(int eventId);
+        bool EditEvent(Event editedEvent);
+    }
+}
